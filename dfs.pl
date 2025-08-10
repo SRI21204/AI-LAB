46083ce72dcfd1ecbde152c1b7d6978e127dadb6
SRI21204 <114294742+SRI21204@users.noreply.github.com>
2025-08-10 14:01:18 +0530

    Update dfs.pl

--- dfs.pl
+++ dfs.pl
@@ -1,42 +1,10 @@
 /* To run the program
 [dfs].
-solve(a,Solution).
+path(a,P).
 use ; colon to get more outputs
 */
-% Define the goal node
-goal(g).  % <-- You can change 'g' to your actual goal node
+goal(g).
+s(a,b). s(b,c). s(c,g). s(a,d). s(d,e). s(e,g).
 
-% Define state transitions (edges in the graph)
-% s(Node, NextNode) represents a directed edge from Node to NextNode
-s(a, b).
-s(b, c).
-s(c, g).
-s(a, d).
-s(d, e).
-s(e, g).
-
-% solve(Node, Solution):
-% Solution is a path (in reverse order) from Node to a goal node
-solve(Node, Solution) :-
-    depthfirst([], Node, Solution).
-
-% depthfirst(Path, Node, Solution):
-% Builds Solution by extending the path [Node | Path] to a goal
-depthfirst(Path, Node, [Node | Path]) :-
-    goal(Node).
-
-depthfirst(Path, Node, Solution) :-
-    s(Node, Next),
-    \+ member(Next, Path),        % Avoid cycles
-    depthfirst([Node | Path], Next, Solution).
-
-% depthfirst2(Node, Path, MaxDepth):
-% Bounded Depth-First Search variant
-depthfirst2(Node, [Node], _) :-
-    goal(Node).
-
-depthfirst2(Node, [Node | Path], MaxDepth) :-
-    MaxDepth > 0,
-    s(Node, Next),
-    NewDepth is MaxDepth - 1,
-    depthfirst2(Next, Path, NewDepth).
+path(Node, [Node]) :- goal(Node).
+path(Node, [Node|Rest]) :- s(Node, Next), path(Next, Rest).
