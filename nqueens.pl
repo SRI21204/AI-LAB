0ed7d5c99f06aeeb5de3d76dee53d6a229a81310
SRI21204 <114294742+SRI21204@users.noreply.github.com>
2025-08-10 17:31:23 +0530

    Update nqueens.pl

--- nqueens.pl
+++ nqueens.pl
@@ -8,17 +8,14 @@ use ; colon to get more outputs
 n_queens(N, Qs) :-
     length(Qs, N),
     Qs ins 1..N,
-    safe_queens(Qs),
-    label(Qs).  % <- Required to generate concrete values
+    all_different(Qs),
+    numlist(1, N, Cs),
+    maplist(sum, Qs, Cs, SumList),
+    all_different(SumList),
+    maplist(diff, Qs, Cs, DiffList),
+    all_different(DiffList),
+    label(Qs).
 
-safe_queens([]).
-safe_queens([Q|Qs]) :-
-    safe_queens(Qs),
-    no_attack(Q, Qs, 1).
+sum(R, C, S) :- S #= R + C.
+diff(R, C, D) :- D #= R - C.
 
-no_attack(_, [], _).
-no_attack(Q, [Q1|Qs], D) :-
-    Q #\= Q1,
-    abs(Q - Q1) #\= D,
-    D1 #= D + 1,
-    no_attack(Q, Qs, D1).
